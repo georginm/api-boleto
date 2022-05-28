955240f17d8bd78d6199a28320da15e1c4595669
georginm <georginmatheus@gmail.com>
2022-05-28 15:00:58 -0300

    add handle errors

--- src/shared/infra/http/express/app.ts
+++ src/shared/infra/http/express/app.ts
@@ -2,9 +2,12 @@ import express from 'express';
 
 import 'express-async-errors';
 import 'dotenv/config';
+import handleErrors from './middleware/handleErrors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(handleErrors);
 
 export { app };
---
+++ src/shared/infra/http/express/middleware/handleErrors.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { AppError } from 'shared/errors/AppError';
+
+export default (err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+};
