185cec68edf463687928d3c84d329446424af7a3
georginm <georginmatheus@gmail.com>
2022-05-30 16:03:36 -0300

    fix: add next function

--- src/shared/infra/http/express/middleware/handleErrors.ts
+++ src/shared/infra/http/express/middleware/handleErrors.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { AppError } from 'shared/errors/AppError';
 
-export default (err: Error, request: Request, response: Response) => {
+export default // eslint-disable-next-line @typescript-eslint/no-unused-vars
+(err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       message: err.message,
