467021491d80a8dbde0b37ab00a8dd66d074c06f
georginm <georginmatheus@gmail.com>
2022-05-31 14:30:09 -0300

    add handleConcessionaireUseCase

--- src/modules/invoices/services/InvoiceService.ts
+++ src/modules/invoices/services/InvoiceService.ts
@@ -3,22 +3,31 @@ import { BadRequestError } from 'shared/errors/BadRequestError';
 import { inject, injectable } from 'tsyringe';
 
 import { IInvoiceDTO } from '../dto/IInvoiceDTO';
+import { IConcessionaireUseCase } from '../useCases/concessionaireInvoiceUseCase/IConcessionaireUseCase';
 import { ITitleUseCase } from '../useCases/titleInvoiceUseCase/ITitleUseCase';
 
 @injectable()
 class InvoiceService {
   constructor(
     @inject('TitleUseCase')
-    private titleUseCases: ITitleUseCase
+    private titleUseCases: ITitleUseCase,
+
+    @inject('ConcessionaireUseCase')
+    private concessionaireUseCase: IConcessionaireUseCase
   ) {}
 
   execute(digitableLine: string): IInvoiceDTO {
     if (!isNumeric(digitableLine)) throw new BadRequestError('O código digitado não é válido');
 
-    // if (digitableLine.length === 47)
-    return this.titleUseCases.handleTitleInvoice(digitableLine);
+    if (digitableLine.length === 47) {
+      return this.titleUseCases.handleTitleInvoice(digitableLine);
+    }
 
-    // return this.handleConcessionaireInvoice(digitableLine); // not implemented
+    if (digitableLine.length === 48) {
+      return this.concessionaireUseCase.handleConcessionaireInvoice(digitableLine);
+    }
+
+    throw new BadRequestError('O código digitado não é válido');
   }
 }
 
--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -1,5 +1,12 @@
+import { IConcessionaireUseCase } from 'modules/invoices/useCases/concessionaireInvoiceUseCase/IConcessionaireUseCase';
+import { ConcessionaireUseCase } from 'modules/invoices/useCases/concessionaireInvoiceUseCase/implementation/ConcessionaireUseCase';
 import { TitleUseCase } from 'modules/invoices/useCases/titleInvoiceUseCase/Implementation/TitleUseCase';
 import { ITitleUseCase } from 'modules/invoices/useCases/titleInvoiceUseCase/ITitleUseCase';
 import { container } from 'tsyringe';
 
 container.registerSingleton<ITitleUseCase>('TitleUseCase', TitleUseCase);
+
+container.registerSingleton<IConcessionaireUseCase>(
+  'ConcessionaireUseCase',
+  ConcessionaireUseCase
+);
